5a2ebfbb6f6c9dd525b4122a5a359f6ee1683d9d
maksimbaranov <maksim.bb52@gmail.com>
2025-01-30 21:13:21 +0300

    123

--- src/main/java/com/example/kiselev/demo/micros1/controller/CustomerController.java
+++ src/main/java/com/example/kiselev/demo/micros1/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.kiselev.demo.micros1.controller;
 import com.example.kiselev.demo.micros1.model.CustomerDTO;
 import com.example.kiselev.demo.micros1.service.CustomerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CustomerController {
-    @Autowired
-    private CustomerService customerService;
+
+    private final CustomerService customerService;
 
     @PostMapping
     public void addCustomer(@RequestParam int id, @RequestParam String name) {
